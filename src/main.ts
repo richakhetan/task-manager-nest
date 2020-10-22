67a004c3ba34100bd165f017f4fb5354b6cd9ce9
Richa Khetan <richa.khetan22@gmail.com>
2020-10-23 02:15:29 +0530

    Env Setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env.PORT)
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
